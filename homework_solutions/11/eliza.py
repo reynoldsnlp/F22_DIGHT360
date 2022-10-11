b36357e9ac9cf41adfc6d78a9fc8e232ac3446d0
reynoldsnlp <reynoldsnlp@users.noreply.github.com>
2022-10-11 13:57:13 -0600

    comments

--- homework_solutions/11/eliza.py
+++ homework_solutions/11/eliza.py
@@ -9,19 +9,18 @@ nlp = spacy.load('en_core_web_sm')
 
 def process_input(input_str):
     doc = nlp(input_str)
-    eat_toks = []
-    for token in doc:
-        if token.lemma_ in {'eat', 'binge'}:
-            eat_toks.append(token)
+    eat_toks = []                                          # EX A
+    for token in doc:                                      # EX A
+        if token.lemma_ in {'eat', 'binge'}:               # EX A
+            eat_toks.append(token)                         # EX A
     if re.search('happy', input_str, flags=re.IGNORECASE):
         return "I'm glad to hear it. What makes you happy?"
     elif input_str.lower() in {'quit', 'exit', 'q', 'stop'}:
         return  # equivalent to `return None`
-    elif eat_toks:
-        return f"Talk more about {eat_toks[0].lemma_}ing."
-    elif toks := [t for t in doc if t.lemma_ in {'eat', 'binge'}]:
-        print(toks)
-        return "Talk more about eating."
+    elif eat_toks:                                         # EX A
+        return f"Talk more about {eat_toks[0].lemma_}ing." # EX A
+    elif toks := [t for t in doc if t.lemma_ in {'eat', 'binge'}]:  # EX B using the walrus operator and a list comprehension
+        return f"Talk more about {toks[0].lemma_}ing."              # EX B
     else:
         return "Tell me more about that."
 
